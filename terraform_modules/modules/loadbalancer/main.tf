591183f2053a5aaa20a77205fe3f62bfa2eb20ca
ogbunugafor-philip <ogbunugafor.philiposita@gmail.com>
2025-04-19 00:06:59 -0700

    update

--- terraform_modules/modules/loadbalancer/main.tf
+++ terraform_modules/modules/loadbalancer/main.tf
@@ -1,16 +1,3 @@
-resource "aws_acm_certificate" "ssl_cert" {
-  domain_name       = "philipogbunugafor.com"
-  validation_method = "DNS"
-
-  tags = {
-    Name = "App SSL Certificate"
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
 resource "aws_lb" "app_alb" {
   name               = "app-alb"
   internal           = false
@@ -48,19 +35,6 @@ resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.app_alb.arn
   port              = 80
   protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app_tg.arn
-  }
-}
-
-resource "aws_lb_listener" "https" {
-  load_balancer_arn = aws_lb.app_alb.arn
-  port              = 443
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = aws_acm_certificate.ssl_cert.arn
 
   default_action {
     type             = "forward"
--- terraform_modules/modules/loadbalancer/outputs.tf
+++ terraform_modules/modules/loadbalancer/outputs.tf
@@ -2,13 +2,3 @@ output "alb_dns_name" {
   description = "DNS name of the Application Load Balancer"
   value       = aws_lb.app_alb.dns_name
 }
-
-output "target_group_arn" {
-  description = "ARN of the Target Group"
-  value       = aws_lb_target_group.app_tg.arn
-}
-
-output "certificate_arn" {
-  description = "ARN of the ACM certificate"
-  value       = aws_acm_certificate.ssl_cert.arn
-}
